5740331df3acf844ff8c91ee4c38ea5e2d593fce
Ganesh Singh <gzesingh1432@gmail.com>
2024-10-05 19:00:24 +0530

    rout hander

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
 
-//request handler
-app.use("/test", (req, res) => {
-  res.send("hellow");
-});
-app.use("/", (req, res) => {
-  res.send("bla vla bla waazzup 🧑‍💻");
-});
+app.use(
+  "/user",
+ [(req, res, next) => {
+    console.log("handelling the rought user");
+
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 2");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("second route handler");
+    console.log("handler 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 4");
+    // res.send("second route handler");
+
+    next();
+  },
+  (req, res, next) => {
+    // res.send("second route handler");
+    console.log("handler 5");
+    res.send("last  amigo");
+    // next();
+  }]
+);
 
 app.listen(3000, () => {
   console.log("server is listening on port no 3000");
---
+++ task.md
@@ -0,0 +1,8 @@
+express notes
+- app.use("/") it is a wild card route matching in this the positin of the code matters a lot
+
+Http protocol
+- put
+- patch
+- get 
+- delete    
